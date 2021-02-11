02459a449da932e5546d2bbdde71da5beccc460a
NishidhJain <nishidhjain909@gmail.com>
2021-02-11 21:50:08 +0530

    updated styling of weather component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [data, setData] = React.useState({})
   const API_KEY = '70abe5234a96fd06a01426ddd67f11c1';
   const url = 'https://api.openweathermap.org/data/2.5/weather?q=';
+  const oneCallAPI_URL = 'https://api.openweathermap.org/data/2.5/onecall';
 
   // console.log(data.length);
   const getData = async (query) => {
@@ -21,6 +22,18 @@ function App() {
       const jsonResponse = await res.json();
       console.log('json response', jsonResponse);
       setData(jsonResponse);
+
+      if (jsonResponse.cod == '200') {
+
+        const { coord } = jsonResponse;
+
+        const oneAPI = `${oneCallAPI_URL}?lat=${coord.lat}&lon=${coord.lon}&appid=${API_KEY}`;
+
+        const oneCallResponse = await fetch(oneAPI);
+        console.log('one call Api', oneCallResponse);
+        const oneCallJSON = await oneCallResponse.json();
+        console.log('one call json', oneCallJSON);
+      }
     }
     catch (err) {
       console.log('err occured', err);
@@ -30,10 +43,31 @@ function App() {
 
 
   return (
-    <div className="App">
-      <h1>Weather Webapp</h1>
-      <Search getData={getData} />
-      { data.cod == '200' ? (<Weather weatherData={data} />) : data.message}
+    <div className="app">
+      <div className="app__container">
+        <div className="app__top">
+
+          <div className="space__container"></div>
+
+          <Search getData={getData} />
+
+          {data.cod == '200' ? (<Weather weatherData={data} />) : data.message}
+
+
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="wave">
+            <path fill="#fff" fill-opacity="1" d="M0,64L34.3,101.3C68.6,139,137,213,206,250.7C274.3,288,343,288,411,256C480,224,549,160,617,154.7C685.7,149,754,203,823,234.7C891.4,267,960,277,1029,250.7C1097.1,224,1166,160,1234,160C1302.9,160,1371,224,1406,256L1440,288L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
+          </svg>
+
+          {/* <Search /> */}
+        </div>
+      </div>
+
+      {/* <h1>Weather Webapp</h1> */}
+      <div className="app-container rain">
+
+
+      </div>
+
 
     </div>
   );
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { TiWeatherPartlySunny } from 'react-icons/ti'
 import '../css/Weather.css';
 
 function Weather({ weatherData }) {
@@ -8,24 +9,42 @@ function Weather({ weatherData }) {
     const iconURL = 'http://openweathermap.org/img/wn/';
 
     const { main, weather, wind, name, dt } = weatherData;
-    const dat = new Date(parseInt(dt));
+    const dat = new Date(parseInt(dt * 1000));
+    const hours = dat.getHours();
+    const minutes = dat.getMinutes();
+    const seconds = dat.getSeconds();
     console.log('date : ', dat);
+    console.log('hr ', hours);
+    console.log('min', minutes);
+
+    console.log('sec ', seconds);
+
 
     if (weather[0].main == 'rain') {
         bgImg = 'rain';
     }
 
     return (
-        <div className="weather rain">
+        <div className="weather">
             <div className="weather__mainData">
-                <h2 className="weather__temp">{main.temp}<sup>&deg;c</sup></h2>
-                <h3 className="weather__city">{name}</h3>
-                <h4 className="weather__condition">{weather[0].description}</h4>
-                <img src={`${iconURL}${weather[0].icon}@2x.png`} alt="weather_icon" className="weather__icon" />
-                <h4 className="waether__humidity">{main.humidity}%</h4>
+                <div className="left">
+
+                    <TiWeatherPartlySunny className="weather__icon" />
+                    <h4 className="weather__condition">{weather[0].description}</h4>
+                </div>
+                <div className="right">
+                    <h2 className="weather__city">{name}</h2>
+                    <h2 className="weather__temp">{main.temp}<sup>&deg;</sup></h2>
+
+                </div>
+
+                {/* <img src={`${iconURL}${weather[0].icon}@2x.png`} alt="weather_icon" className="weather__icon" />
+                <h4 className="waether__humidity">{main.humidity}%</h4> */}
             </div>
-            <div className="weather__windData">
-                <div className="weather__windSpeed">Wind : {wind.speed} meter/sec</div>
+            <div className="weather__otherData">
+                <p className="weather__windSpeed">Wind : {wind.speed} m/sec</p>
+                <p className="weather__humidity">Humidity : {main.humidity}%</p>
+                <p className="weather__feelsLike">Feels : {main.feels_like} <sup>&deg;</sup> </p>
             </div>
         </div>
     )
