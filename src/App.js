7178424635e95d0899402b525bbf6eea790418ee
NishidhJain <nishidhjain909@gmail.com>
2021-06-24 14:10:14 +0530

    current city data will be fetched on initial render

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import React from 'react';
-import Search from './components/Search';
-import './App.css';
-import Weather from './components/Weather';
-import DailyForecast from './components/DailyForecast';
+import React, { useState, useEffect } from "react";
+import Search from "./components/Search";
+import "./App.css";
+import Weather from "./components/Weather";
+import DailyForecast from "./components/DailyForecast";
 
 function App() {
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const [data, setData] = useState({});
+  const [forecastData, setForecastData] = useState(null);
+  const [error, setError] = useState("");
 
-  const [data, setData] = React.useState({});
-  const [forecastData, setForecastData] = React.useState(null);
-  const [error, setError] = React.useState('');
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=";
+  const oneCallAPI_URL = "https://api.openweathermap.org/data/2.5/onecall";
+  const findCity = "https://api.openweathermap.org/data/2.5/weather?";
 
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q=';
-  const oneCallAPI_URL = 'https://api.openweathermap.org/data/2.5/onecall';
+  useEffect(() => {
+    getCurrentLocationData();
+  }, []);
 
   // console.log(process.env);
+  // const getCurrentLocationData = () => {
+  //   // check if browser supports geolocation
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(async ({ coords }) => {
+  //       const { latitude, longitude } = coords;
+  //       // console.log(latitude, longitude);
+  //       const findCityURL = `${findCity}lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_API_KEY}`;
+  //       try {
+  //         const currentCityURL = `${oneCallAPI_URL}?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,alerts&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
+  //         console.log(currentCityURL);
+  //         const currentCityData = await fetch(currentCityURL);
+  //         const currentCityJSON = await currentCityData.json();
+  //         console.log("Current location data: ", currentCityJSON);
+  //       } catch (err) {
+  //         console.log("Err in fetching current city data: ", err);
+  //       }
+  //     });
+  //   } else {
+  //     alert("Geolocation is not supported by this browser.");
+  //   }
+  // };
+
+  const getCurrentLocationData = () => {
+    // check if browser supports geolocation
+    if (navigator.geolocation) {
+      console.log("This browser supports Geolocation");
+
+      navigator.geolocation.getCurrentPosition(async ({ coords }) => {
+        const { latitude, longitude } = coords;
+        console.log(latitude, longitude);
+
+        const findCityURL = `${findCity}lat=${latitude}&lon=${longitude}&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
+
+        try {
+          const cityDetails = await fetch(findCityURL);
+          const cityDetailsJSON = await cityDetails.json();
+          // console.log(cityDetailsJSON);
+          setData(cityDetailsJSON);
+
+          // if data is found then fetch forecast data
+          if (cityDetailsJSON.cod == "200") {
+            const oneAPI = `${oneCallAPI_URL}?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,alerts&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
+
+            const forecastData = await fetch(oneAPI);
+            const forecastDataJSON = await forecastData.json();
+            // console.log(forecastDataJSON);
+            setForecastData(forecastDataJSON);
+          } else {
+            setForecastData(null);
+          }
+        } catch (err) {
+          console.log("Err in fetching current city details: ", err);
+        }
+      });
+    } else {
+      console.log("This browser does not support Geolocation");
+    }
+  };
 
   // console.log(data.length);
   const getData = async (query) => {
-
     // reset the error
-    setError('');
+    setError("");
     // console.log('get Data called, query is:', query);
 
     const queryURL = `${url}${query}&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
@@ -28,11 +91,10 @@ function App() {
       const res = await fetch(queryURL);
       // console.log('response received', res);
       const jsonResponse = await res.json();
-      console.log('json response', jsonResponse);
+      console.log("json response", jsonResponse);
       setData(jsonResponse);
 
-      if (jsonResponse.cod == '200') {
-
+      if (jsonResponse.cod == "200") {
         const { coord } = jsonResponse;
 
         const oneAPI = `${oneCallAPI_URL}?lat=${coord.lat}&lon=${coord.lon}&exclude=minutely,hourly,alerts&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
@@ -41,68 +103,75 @@ function App() {
         // console.log('one call Api', oneCallResponse);
         const oneCallJSON = await oneCallResponse.json();
         setForecastData(oneCallJSON);
-        console.log('one call json', oneCallJSON);
-      }
-      else {
+        console.log("one call json", oneCallJSON);
+      } else {
         // setData({})
         setForecastData(null);
       }
-    }
-    catch (err) {
-      console.log('err occured', err.message);
+    } catch (err) {
+      console.log("err occured", err.message);
       // covert the err message to lowercase and check for network err
       const errMsg = err.message.toLowerCase();
       console.log(errMsg);
 
       // check if the string exist in the message, if not then '-1'
-      if (errMsg.search('networkerror') >= 0) {
-        console.log('inside if');
-        setError('Network Error Occured');
-      }
-      else {
-        console.log('inside else');
+      if (errMsg.search("networkerror") >= 0) {
+        console.log("inside if");
+        setError("Network Error Occured");
+      } else {
+        console.log("inside else");
       }
     }
-  }
+  };
   // console.log('data value updated', data);
-
 
   return (
     <div className="app">
       <div className="app__container">
         <div className="app__top">
-
           <div className="space__container"></div>
 
           <Search getData={getData} />
 
-          {data.cod == '200' ? (<Weather weatherData={data} />) :
-            (<div className="error__container">
+          {data.cod == "200" ? (
+            <Weather weatherData={data} />
+          ) : (
+            <div className="error__container">
               <h3 className="error__msg">{data.message}</h3>
-            </div>)
-          }
-          {
-            error ? (
-              <div className="networkErr error__container">
-                <h3 className="networkErr__msg">{error}</h3>
-                <p className="networkErr__warning">Please check your internet connection</p>
-              </div>
-            ) : ''
-          }
+            </div>
+          )}
+          {error ? (
+            <div className="networkErr error__container">
+              <h3 className="networkErr__msg">{error}</h3>
+              <p className="networkErr__warning">
+                Please check your internet connection
+              </p>
+            </div>
+          ) : (
+            ""
+          )}
 
-
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="wave">
-            <path fill="#fff" fillOpacity="1" d="M0,64L34.3,101.3C68.6,139,137,213,206,250.7C274.3,288,343,288,411,256C480,224,549,160,617,154.7C685.7,149,754,203,823,234.7C891.4,267,960,277,1029,250.7C1097.1,224,1166,160,1234,160C1302.9,160,1371,224,1406,256L1440,288L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+            className="wave"
+          >
+            <path
+              fill="#fff"
+              fillOpacity="1"
+              d="M0,64L34.3,101.3C68.6,139,137,213,206,250.7C274.3,288,343,288,411,256C480,224,549,160,617,154.7C685.7,149,754,203,823,234.7C891.4,267,960,277,1029,250.7C1097.1,224,1166,160,1234,160C1302.9,160,1371,224,1406,256L1440,288L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"
+            ></path>
           </svg>
 
           {/* <Search /> */}
         </div>
 
         <div className="app__bottom">
-
-
-          {forecastData ? (<DailyForecast forecastData={forecastData} />) : ''}
-
+          {forecastData ? (
+            <DailyForecast forecastData={forecastData} />
+          ) : (
+            "No Data Found"
+          )}
 
           {/* <ul className="daily__forecast__list">
           </ul> */}
@@ -110,9 +179,6 @@ function App() {
       </div>
 
       {/* <h1>Weather Webapp</h1> */}
-
-
-
     </div>
   );
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,53 +1,55 @@
-import React from 'react';
-import { TiWeatherPartlySunny } from 'react-icons/ti'
-import '../css/Weather.css';
+import React from "react";
+import { TiWeatherPartlySunny } from "react-icons/ti";
+import "../css/Weather.css";
 
 function Weather({ weatherData }) {
+  // destructure the weatherData object
+  let bgImg = "";
+  const iconURL = "http://openweathermap.org/img/wn/";
+  console.log(weatherData);
+  const { main, weather, wind, name, dt } = weatherData;
+  const dat = new Date(parseInt(dt * 1000));
+  const hours = dat.getHours();
+  const minutes = dat.getMinutes();
+  const seconds = dat.getSeconds();
+  console.log("date : ", dat);
+  console.log("hr ", hours);
+  console.log("min", minutes);
 
-    // destructure the weatherData object
-    let bgImg = '';
-    const iconURL = 'http://openweathermap.org/img/wn/';
+  console.log("sec ", seconds);
 
-    const { main, weather, wind, name, dt } = weatherData;
-    const dat = new Date(parseInt(dt * 1000));
-    const hours = dat.getHours();
-    const minutes = dat.getMinutes();
-    const seconds = dat.getSeconds();
-    console.log('date : ', dat);
-    console.log('hr ', hours);
-    console.log('min', minutes);
+  if (weather[0].main == "rain") {
+    bgImg = "rain";
+  }
 
-    console.log('sec ', seconds);
-
-
-    if (weather[0].main == 'rain') {
-        bgImg = 'rain';
-    }
-
-    return (
-        <div className="weather">
-            <div className="weather__mainData">
-                <div className="left">
-                    {/* <img src={`${iconURL}${weather[0].icon}@2x.png`} alt="weather_icon" className="weather__icon" /> */}
-                    <TiWeatherPartlySunny className="weather__icon" />
-                    <h4 className="weather__condition">{weather[0].description}</h4>
-                </div>
-                <div className="right">
-                    <h2 className="weather__city">{name}</h2>
-                    <h2 className="weather__temp">{main.temp}<sup>&deg;</sup></h2>
-
-                </div>
-
-                {/* 
-                <h4 className="waether__humidity">{main.humidity}%</h4> */}
-            </div>
-            <div className="weather__otherData">
-                <p className="weather__windSpeed">Wind : {wind.speed} m/sec</p>
-                <p className="weather__humidity">Humidity : {main.humidity}%</p>
-                <p className="weather__feelsLike">Feels : {main.feels_like} <sup>&deg;</sup> </p>
-            </div>
+  return (
+    <div className="weather">
+      <div className="weather__mainData">
+        <div className="left">
+          {/* <img src={`${iconURL}${weather[0].icon}@2x.png`} alt="weather_icon" className="weather__icon" /> */}
+          <TiWeatherPartlySunny className="weather__icon" />
+          <h4 className="weather__condition">{weather[0].description}</h4>
         </div>
-    )
+        <div className="right">
+          <h2 className="weather__city">{name}</h2>
+          <h2 className="weather__temp">
+            {main.temp}
+            <sup>&deg;</sup>
+          </h2>
+        </div>
+
+        {/* 
+                <h4 className="waether__humidity">{main.humidity}%</h4> */}
+      </div>
+      <div className="weather__otherData">
+        <p className="weather__windSpeed">Wind : {wind.speed} m/sec</p>
+        <p className="weather__humidity">Humidity : {main.humidity}%</p>
+        <p className="weather__feelsLike">
+          Feels : {main.feels_like} <sup>&deg;</sup>{" "}
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default Weather
+export default Weather;
